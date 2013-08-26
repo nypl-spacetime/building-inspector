79073a747e4c90901c5960a11cfc03d7d507db2c
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2013-08-26 16:55:24 -0400

    precompiling is_building.coffee

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Webappmini
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile += ['is_building.coffee']
+    config.assets.precompile += ['is_building.js']
 
     config.assets.paths << Rails.root.join("app", "assets", "files")
   end
