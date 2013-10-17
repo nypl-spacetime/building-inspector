f36b493fbd72091a12a6b691d4cecfc6afa85f51
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2013-10-17 17:59:26 -0400

    removed registration in login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  # :validatable, :recoverable, :trackable, 
-  devise :database_authenticatable, :registerable, :rememberable,
+  # :validatable, :recoverable, :trackable, :registerable, 
+  devise :database_authenticatable, :rememberable,
          :omniauthable, :omniauth_providers => [:google_oauth2, :facebook, :twitter]
          
   has_many :usersessions
