4ddb48da73f7ab99b60e6d629d2fd646d2a42c76
David Riordan <david.riordan@gmail.com>
2013-10-22 17:34:51 -0400

    added manifest for FirefoxOS webapp

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,7 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+#.webapp is the Mozilla Firefox manifest format
+#https://developer.mozilla.org/en-US/Apps/Developing/Manifest
+Rack::Mime::MIME_TYPES['.webapp'] = 'application/x-web-app-manifest+json'
