911d6036cc5ca25ce8f1c073ae16b0584529c743
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2013-10-28 19:07:47 -0400

    changed centroid to lat lon

--- db/migrate/20131028225506_remove_centroid_from_polygon.rb
+++ db/migrate/20131028225506_remove_centroid_from_polygon.rb
@@ -1,7 +1,7 @@
 class RemoveCentroidFromPolygon < ActiveRecord::Migration
   def change
   	remove_column :polygons, :centroid
-  	add_column :polygons, :centroid_lat, :float
-  	add_column :polygons, :centroid_lon, :float
+  	add_column :polygons, :centroid_lat, :decimal, :precision => 15, :scale => 12
+  	add_column :polygons, :centroid_lon, :decimal, :precision => 15, :scale => 12
   end
 end
