15868badb2b1fe0494aa93a04753b7b66f3e2622
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2013-11-22 12:05:22 -0500

    added type to flag methods

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -5,40 +5,40 @@ class Flag < ActiveRecord::Base
 	validates :polygon_id, presence: true
 	validates :flag_type, presence: true
 
-	def self.flags_for_sheet_for_session(sheet_id, session_id)
+	def self.flags_for_sheet_for_session(sheet_id, session_id, type = "geometry")
 		# just need the count
-		Flag.select('DISTINCT flags.polygon_id, flags.flag_value, polygons.geometry, polygons.sheet_id, polygons.dn').joins([polygon: :sheet]).where('sheets.id = ? AND flags.session_id = ?', sheet_id, session_id)
+		Flag.select('DISTINCT flags.polygon_id, flags.flag_value, polygons.geometry, polygons.sheet_id, polygons.dn').joins([polygon: :sheet]).where('sheets.id = ? AND flags.session_id = ? AND flags.flag_type = ?', sheet_id, session_id, type)
 	end
 
-	def self.flags_for_sheet_for_user(sheet_id, user_id)
+	def self.flags_for_sheet_for_user(sheet_id, user_id, type = "geometry")
 		# just need the count
-		Flag.select('DISTINCT flags.polygon_id, flags.flag_value, polygons.geometry, polygons.sheet_id, polygons.dn').joins([polygon: :sheet]).joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('sheets.id = ? AND usersessions.user_id = ?', sheet_id, user_id)
+		Flag.select('DISTINCT flags.polygon_id, flags.flag_value, polygons.geometry, polygons.sheet_id, polygons.dn').joins([polygon: :sheet]).joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('sheets.id = ? AND usersessions.user_id = ? AND flags.flag_type = ?', sheet_id, user_id, type)
 	end
 
-	def self.grouped_flags_for_session(session_id)
+	def self.grouped_flags_for_session(session_id, type = "geometry")
 		# just need the count per sheet
-		Flag.select('COUNT(DISTINCT flags.polygon_id) as flag_count, sheets.id, sheets.bbox').joins([polygon: :sheet]).where('flags.session_id = ?', session_id).group("sheets.id")
+		Flag.select('COUNT(DISTINCT flags.polygon_id) as flag_count, sheets.id, sheets.bbox').joins([polygon: :sheet]).where('flags.session_id = ? AND flags.flag_type = ?', session_id, type).group("sheets.id")
 	end
 
-	def self.grouped_flags_for_user(user_id)
+	def self.grouped_flags_for_user(user_id, type = "geometry")
 		# just need the count per sheet
-		Flag.select('COUNT(DISTINCT flags.polygon_id) as flag_count, sheets.id, sheets.bbox').joins([polygon: :sheet]).joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('usersessions.user_id = ?', user_id).group("sheets.id")
+		Flag.select('COUNT(DISTINCT flags.polygon_id) as flag_count, sheets.id, sheets.bbox').joins([polygon: :sheet]).joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('usersessions.user_id = ? AND flags.flag_type = ?', user_id, type).group("sheets.id")
 	end
 
-	def self.flags_for_session(session_id)
-		Flag.select("DISTINCT polygon_id").where(:session_id => session_id).count
+	def self.flags_for_session(session_id, type = "geometry")
+		Flag.select("DISTINCT polygon_id").where("session_id = ? AND flag_type = ?", session_id, type).count
 	end
 	
-	def self.flags_for_user(user_id)
-		Flag.select("DISTINCT polygon_id").joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('usersessions.user_id = ?', user_id).count    
+	def self.flags_for_user(user_id, type = "geometry")
+		Flag.select("DISTINCT polygon_id").joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('usersessions.user_id = ? AND flags.flag_type = ?', user_id, type).count    
 	end
 
-	def self.progress_for_session(session_id)
-		Flag.select("DISTINCT polygons.id, polygons.centroid_lat, polygons.centroid_lon, polygons.geometry, flags.*").joins(:polygon).where(:session_id => session_id)
+	def self.progress_for_session(session_id, type = "geometry")
+		Flag.select("DISTINCT polygons.id, polygons.centroid_lat, polygons.centroid_lon, polygons.geometry, flags.*").joins(:polygon).where("flags.session_id = ? AND flags.flag_type = ?", session_id, type)
 	end
 	
-	def self.progress_for_user(user_id)
-		Flag.select("DISTINCT polygons.id, polygons.centroid_lat, polygons.centroid_lon, polygons.geometry, flags.*").joins(:polygon).joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('usersessions.user_id = ?', user_id)
+	def self.progress_for_user(user_id, type = "geometry")
+		Flag.select("DISTINCT polygons.id, polygons.centroid_lat, polygons.centroid_lon, polygons.geometry, flags.*").joins(:polygon).joins('INNER JOIN usersessions ON usersessions.session_id = flags.session_id').where('usersessions.user_id = ? AND flags.flag_type = ?', user_id, type)
 	end
   
 end
