fab36efe375ae97a93f8c565178439a0d70f50ff
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2014-03-24 15:45:09 -0400

    commented out the consensus migration since we will be re-creating the whole thing from scratch

--- db/migrate/20140320154554_move_polygon_consensus_to_consensus_table.rb
+++ db/migrate/20140320154554_move_polygon_consensus_to_consensus_table.rb
@@ -1,14 +1,14 @@
 class MovePolygonConsensusToConsensusTable < ActiveRecord::Migration
   def up
     # move data
-    Polygon.find_each do |polygon|
-      if polygon.consensus != nil
-        polygon.consensuspolygons.create(
-        :consensus => polygon.consensus,
-        :task => "geometry",
-        :polygon_id => polygon.id
-        )
-      end
-    end
+    # Polygon.find_each do |polygon|
+    #   if polygon.consensus != nil
+    #     polygon.consensuspolygons.create(
+    #     :consensus => polygon.consensus,
+    #     :task => "geometry",
+    #     :polygon_id => polygon.id
+    #     )
+    #   end
+    # end
   end
 end
