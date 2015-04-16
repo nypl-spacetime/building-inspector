88a14fa207fe78a188fca62dd1394a2c5e871d27
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-04-16 15:15:01 -0400

    :space_invader: fixed epsilon in polygonfix consensus - should give better results / refactoring

--- app/services/consensus_utils.rb
+++ app/services/consensus_utils.rb
@@ -1,5 +1,6 @@
 class ConsensusUtils
 
+  # POLYGONFIX CONSENSUS
   def self.calculate_polygonfix_consensus(geojson)
     # TODO: still lacks a more robust validation/checking
     output = []
@@ -25,130 +26,6 @@ class ConsensusUtils
       output.push(poly)
     end
     return output
-  end
-
-  def self.calculate_address_consensus(flags)
-    # flags are an activerecord list of 'address' type flags for a given sheet
-    # cluster them flags
-    clusters = cluster_addresses(flags)
-    consensus_list = []
-    clusters.each do |c|
-      next if c[0] == -1
-      consensus = address_cluster_consensus(c[1], flags)
-      consensus_list.push(consensus) if consensus != nil
-    end
-    # group by flaggable_id
-    ids = consensus_list.map {|f| f[:flaggable_id]}.uniq
-    grouped_list = {}
-    ids.each do |id|
-      items = consensus_list.select {|x| x[:flaggable_id] == id}
-      grouped_list[id] = items
-    end
-    return grouped_list
-  end
-
-  def self.cluster_addresses(addresses, epsilon=1.425e-05, min_points=2)
-    simple_array = addresses.map { |a| [a["longitude"].to_f, a["latitude"].to_f] }
-    dbscan = DBSCAN( simple_array, :epsilon => epsilon, :min_points => min_points, :distance => :euclidean_distance )
-    return dbscan.results.select{|k,v| k != -1} # omit the non-cluster
-  end
-
-  def self.address_cluster_consensus(cluster, flags, min_count = 3, threshold = 0.75)
-    return nil if cluster.count < min_count
-    flags = get_address_flags_for_cluster(cluster, flags)
-    total_votes = 0
-    address_tally = {} # saves the address popularity
-    id_tally = {} # saves the flaggable_id popularity
-    session_ids = []
-    flags.each do |vote|
-      value = vote["flag_value"]
-      id = vote["flaggable_id"]
-      sid = vote["session_id"]
-      # ignore vote if session_id already exists
-      # to reduce trolling
-      if session_ids.index(sid) != nil
-        next
-      end
-      session_ids.push(sid)
-      # now tally values
-      if address_tally[value] == nil
-        address_tally[value] = 0
-      end
-      if id_tally[id] == nil
-        id_tally[id] = 0
-      end
-      address_tally[value] = address_tally[value] + 1
-      id_tally[id] = id_tally[id] + 1
-      total_votes = total_votes + 1
-    end
-    # in case there was trolling
-    return if total_votes < min_count
-    # sort tally by value
-    address_tally_sorted = address_tally.sort_by { |value,votes| votes }
-    # sort tally by value
-    id_tally_sorted = id_tally.sort_by { |id,count| count }
-    # and the winner is...
-    winner_address = address_tally_sorted.last
-    winner_id = id_tally_sorted.last
-    votes = winner_address[1].to_i
-    consensus = votes.to_f / total_votes.to_f
-    flaggable_id = winner_id[0].to_i
-    flag_value = winner_address[0]
-    # lat/lon is average of points
-    latitude = cluster.map {|c| c[1]}.mean
-    longitude = cluster.map {|c| c[0]}.mean
-    # check if consensus is above threshold
-    return if consensus < threshold
-    winner_mark = {}
-    winner_mark[:latitude] = latitude
-    winner_mark[:longitude] = longitude
-    winner_mark[:flag_value] = flag_value
-    winner_mark[:votes] = votes
-    winner_mark[:total_votes] = total_votes
-    winner_mark[:flaggable_id] = flaggable_id
-    return winner_mark
-  end
-
-  def self.get_address_flags_for_cluster(cluster, flags)
-    output = []
-    cluster.each do |point|
-      flag = get_address_flag_for_point(point, flags)
-      output.push(flag) if flag != nil
-    end
-    return output
-  end
-
-  def self.get_address_flag_for_point(point, flags)
-    flags.each do |f|
-      lat = f["latitude"].to_f
-      lon = f["longitude"].to_f
-      return f if point[0] == lon and point[1] == lat
-    end
-    return nil
-  end
-
-  def self.parse_geojson(json)
-    RGeo::GeoJSON.decode(json, :json_parser => :json)
-  end
-
-  def self.get_centroid(poly_feature)
-    return if (poly_feature.geometry.geometry_type.type_name != "Polygon")
-    c = poly_feature.geometry.centroid
-    return [c.x, c.y]
-  end
-
-  def self.get_all_centroids(geom)
-    centroids = {}
-    geom.each_with_index do |poly,index|
-      next if (poly.geometry.geometry_type.type_name != "Polygon")
-      centroids[index] = get_centroid(poly)
-    end
-    return centroids
-  end
-
-  def self.cluster_centroids(centroids, epsilon=1.8e-06, min_points=2)
-    dbscan = DBSCAN( centroids.map{|c| c[1]}, :epsilon => epsilon, :min_points => min_points, :distance => :euclidean_distance )
-    return dbscan.results.select{|k,v| k != -1} # omit the non-cluster
   end
 
   # given a list of centroids (lon,lat), find their poly's index in the centroid list (index => lon,lat)
@@ -181,7 +58,7 @@ class ConsensusUtils
   end
 
   # perform point clustering EXCLUDING first item in each poly since it is same as last
-  def self.cluster_points(points, epsilon=5.5e-06, min_points=2)
+  def self.cluster_points(points, epsilon=3.5e-06, min_points=2)
     dbscan = DBSCAN( points.flatten(1), :epsilon => epsilon, :min_points => min_points, :distance => :euclidean_distance )
     return dbscan.results.select{|k,v| k != -1} # omit the non-cluster
   end
@@ -282,6 +159,133 @@ class ConsensusUtils
 
   def self.consensus_to_geojson(consensus, id)
     {:type => "FeatureCollection", :features => consensus.map { |f| {:type => "Feature", :properties => { :flaggable_id => id }, :geometry => { :type => "Polygon", :coordinates =>[f] } } } }.to_json
+  end
+
+  def self.get_centroid(poly_feature)
+    return if (poly_feature.geometry.geometry_type.type_name != "Polygon")
+    c = poly_feature.geometry.centroid
+    return [c.x, c.y]
+  end
+
+  def self.get_all_centroids(geom)
+    centroids = {}
+    geom.each_with_index do |poly,index|
+      next if (poly.geometry.geometry_type.type_name != "Polygon")
+      centroids[index] = get_centroid(poly)
+    end
+    return centroids
+  end
+
+  # ADDRESS CONSENSUS
+  def self.calculate_address_consensus(flags)
+    # flags are an activerecord list of 'address' type flags for a given sheet
+    # cluster them flags
+    clusters = cluster_addresses(flags)
+    consensus_list = []
+    clusters.each do |c|
+      next if c[0] == -1
+      consensus = address_cluster_consensus(c[1], flags)
+      consensus_list.push(consensus) if consensus != nil
+    end
+    # group by flaggable_id
+    ids = consensus_list.map {|f| f[:flaggable_id]}.uniq
+    grouped_list = {}
+    ids.each do |id|
+      items = consensus_list.select {|x| x[:flaggable_id] == id}
+      grouped_list[id] = items
+    end
+    return grouped_list
+  end
+
+  def self.cluster_addresses(addresses, epsilon=1.425e-05, min_points=2)
+    simple_array = addresses.map { |a| [a["longitude"].to_f, a["latitude"].to_f] }
+    dbscan = DBSCAN( simple_array, :epsilon => epsilon, :min_points => min_points, :distance => :euclidean_distance )
+    return dbscan.results.select{|k,v| k != -1} # omit the non-cluster
+  end
+
+  def self.address_cluster_consensus(cluster, flags, min_count = 3, threshold = 0.75)
+    return nil if cluster.count < min_count
+    flags = get_address_flags_for_cluster(cluster, flags)
+    total_votes = 0
+    address_tally = {} # saves the address popularity
+    id_tally = {} # saves the flaggable_id popularity
+    session_ids = []
+    flags.each do |vote|
+      value = vote["flag_value"]
+      id = vote["flaggable_id"]
+      sid = vote["session_id"]
+      # ignore vote if session_id already exists
+      # to reduce trolling
+      if session_ids.index(sid) != nil
+        next
+      end
+      session_ids.push(sid)
+      # now tally values
+      if address_tally[value] == nil
+        address_tally[value] = 0
+      end
+      if id_tally[id] == nil
+        id_tally[id] = 0
+      end
+      address_tally[value] = address_tally[value] + 1
+      id_tally[id] = id_tally[id] + 1
+      total_votes = total_votes + 1
+    end
+    # in case there was trolling
+    return if total_votes < min_count
+    # sort tally by value
+    address_tally_sorted = address_tally.sort_by { |value,votes| votes }
+    # sort tally by value
+    id_tally_sorted = id_tally.sort_by { |id,count| count }
+    # and the winner is...
+    winner_address = address_tally_sorted.last
+    winner_id = id_tally_sorted.last
+    votes = winner_address[1].to_i
+    consensus = votes.to_f / total_votes.to_f
+    flaggable_id = winner_id[0].to_i
+    flag_value = winner_address[0]
+    # lat/lon is average of points
+    latitude = cluster.map {|c| c[1]}.mean
+    longitude = cluster.map {|c| c[0]}.mean
+    # check if consensus is above threshold
+    return if consensus < threshold
+    winner_mark = {}
+    winner_mark[:latitude] = latitude
+    winner_mark[:longitude] = longitude
+    winner_mark[:flag_value] = flag_value
+    winner_mark[:votes] = votes
+    winner_mark[:total_votes] = total_votes
+    winner_mark[:flaggable_id] = flaggable_id
+    return winner_mark
+  end
+
+  def self.get_address_flags_for_cluster(cluster, flags)
+    output = []
+    cluster.each do |point|
+      flag = get_address_flag_for_point(point, flags)
+      output.push(flag) if flag != nil
+    end
+    return output
+  end
+
+  def self.get_address_flag_for_point(point, flags)
+    flags.each do |f|
+      lat = f["latitude"].to_f
+      lon = f["longitude"].to_f
+      return f if point[0] == lon and point[1] == lat
+    end
+    return nil
+  end
+
+  # GENERIC
+
+  def self.parse_geojson(json)
+    RGeo::GeoJSON.decode(json, :json_parser => :json)
+  end
+
+  def self.cluster_centroids(centroids, epsilon=1.8e-06, min_points=2)
+    dbscan = DBSCAN( centroids.map{|c| c[1]}, :epsilon => epsilon, :min_points => min_points, :distance => :euclidean_distance )
+    return dbscan.results.select{|k,v| k != -1} # omit the non-cluster
   end
 
 end
