4b81eaa1ea4f3e6357d4cb261c5d224dcb99620c
Mauricio Giraldo <mauriciogiraldo@nypl.org>
2015-10-22 16:07:22 -0400

    :space_invader: minor sql change to topo flag map

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -19,7 +19,7 @@ class Flag < ActiveRecord::Base
             s = bbox[1]
             e = bbox[2]
             n = bbox[3]
-            Flag.select('DISTINCT flags.flaggable_id, flags.id, flags.session_id, flags.flag_type, flags.flaggable_type, flags.latitude, flags.longitude, flags.flag_value').joins("INNER JOIN sheets ON sheets.id = flags.flaggable_id").where('(flags.flaggable_id = ? OR (flags.latitude <= ? AND flags.latitude >= ? AND flags.longitude <= ? AND flags.longitude >= ?)) AND flags.session_id = ? AND flags.flag_type = ?', sheet_id, n, s, e, w, session_id, type)
+            Flag.select('DISTINCT flags.flaggable_id, flags.id, flags.session_id, flags.flag_type, flags.flaggable_type, flags.latitude, flags.longitude, flags.flag_value').joins("INNER JOIN sheets ON sheets.id = flags.flaggable_id").where('(sheets.id = ? OR (flags.latitude <= ? AND flags.latitude >= ? AND flags.longitude <= ? AND flags.longitude >= ?)) AND flags.session_id = ? AND flags.flag_type = ?', sheet_id, n, s, e, w, session_id, type)
         end
     end
 
@@ -35,7 +35,7 @@ class Flag < ActiveRecord::Base
             s = bbox[1]
             e = bbox[2]
             n = bbox[3]
-            Flag.select('DISTINCT flags.flaggable_id, flags.id, flags.session_id, flags.flag_type, flags.flaggable_type, flags.latitude, flags.longitude, flags.flag_value').joins("INNER JOIN sheets ON sheets.id = flags.flaggable_id").joins(:usersession).where('(flags.flaggable_id = ? OR (flags.latitude <= ? AND flags.latitude >= ? AND flags.longitude <= ? AND flags.longitude >= ?)) AND usersessions.user_id = ? AND flags.flag_type = ?', sheet_id, n, s, e, w, user_id, type)
+            Flag.select('DISTINCT flags.flaggable_id, flags.id, flags.session_id, flags.flag_type, flags.flaggable_type, flags.latitude, flags.longitude, flags.flag_value').joins("INNER JOIN sheets ON sheets.id = flags.flaggable_id").joins(:usersession).where('(sheets.id = ? OR (flags.latitude <= ? AND flags.latitude >= ? AND flags.longitude <= ? AND flags.longitude >= ?)) AND usersessions.user_id = ? AND flags.flag_type = ?', sheet_id, n, s, e, w, user_id, type)
         end
     end
 
